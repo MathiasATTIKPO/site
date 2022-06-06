eccda0248dd4f1ff5abef899a11e8ada035dfaf7
Mathias ATTIKPO <etudiant_attikpo@outlook.fr>
2022-06-06 19:19:23 +0000

    test3

--- backend/server.js
+++ backend/server.js
@@ -27,11 +27,11 @@ Mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://mathiasattikpo:Mathia
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-/*
+
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);*/
+);
  app.get('/',(req , res) =>{
      res.send('Serveur pret');
  });
