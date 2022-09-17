f94cc9640bd9351bf65dc5b99c280c361fe35f42
Mathias ATTIKPO <etudiant_attikpo@outlook.fr>
2022-09-17 13:43:48 +0000

    version en ligne du chat

--- frontend/src/Screens/SupportScreen.js
+++ frontend/src/Screens/SupportScreen.js
@@ -8,7 +8,7 @@ let allMessages = [];
 let allSelectedUser = {};
 const ENDPOINT =
   window.location.host.indexOf('localhost') >= 0
-    ? 'http://127.0.0.1:5000'
+    ? 'http://localoli.herokuapp.com'
     : window.location.host;
 
 export default function SupportScreen() {
